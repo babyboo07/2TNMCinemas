0946cab9457609bb2dfbd7aff58373ec3b6aa6d8
babyboo07 <mai.pn@aris-vn.com>
2023-03-14 13:21:15 +0700

    API with API login

---
+++ src/AppConstants.js
@@ -0,0 +1,7 @@
+export const URL = "http://localhost:8080/api";
+
+export const ADMIN = 1;
+export const MEMBER = 2;
+
+export const VALIDATIONEMAIL =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;

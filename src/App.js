af576c9a52891343c6a1636a44220c96bdfcc55f
babyboo07 <mai.pn@aris-vn.com>
2023-03-22 15:45:45 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo } from "react";
 
 // react-router components
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation, Router } from "react-router-dom";
 
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
@@ -45,11 +45,8 @@ import CreateRoom from "layouts/room/create";
 import UpdateRoom from "layouts/room/edit";
 import UpdateSeat from "layouts/seat/edit";
 import CreateSeat from "layouts/seat/create";
-<<<<<<< HEAD
 import Home from "layouts/client/home";
-=======
 import CreateMovieDay from "layouts/movieDay/create";
->>>>>>> a7cf07c50b1b32c5d2102339e76078e4fc3c0cad
 
 export default function App() {
   const [controller, dispatch] = useSoftUIController();
@@ -57,6 +54,8 @@ export default function App() {
   const [onMouseEnter, setOnMouseEnter] = useState(false);
   const [rtlCache, setRtlCache] = useState(null);
   const { pathname } = useLocation();
+
+  const [userInfo, setUserInfo] = useState({});
 
   // Cache for the rtl
   useMemo(() => {
@@ -66,6 +65,14 @@ export default function App() {
     });
 
     setRtlCache(cacheRtl);
+  }, []);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user") ? localStorage.getItem("user") : "";
+    if (user) {
+      const dataUser = JSON.parse(user);
+      setUserInfo(dataUser);
+    }
   }, []);
 
   // Open sidenav when mouse enter on mini sidenav
@@ -132,44 +139,53 @@ export default function App() {
       </ThemeProvider>
     </CacheProvider>
   ) : (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {layout === "dashboard" && (
-        <>
-          <Sidenav
-            color={sidenavColor}
-            brand={brand}
-            brandName="2TNMCinemas"
-            routes={routes}
-            onMouseEnter={handleOnMouseEnter}
-            onMouseLeave={handleOnMouseLeave}
-          />
-        </>
-      )}
-      {layout === "vr"}
-      <Routes>
-        {getRoutes(routes)}
-        <Route path="*" element={<Navigate to="/authentication/sign-in" />} />
-        <Route path="/authentication/sign-in" element={<SignIn />} />
-        <Route path="/authentication/sign-up" element={<SignUp />} />
-        <Route path="/movies/create" element={<Createnew />} />
-        <Route path="/movies/edit/:movieId" element={<EditMovie />} />
-        <Route path="/movies/detail/:movieId" element={<DetailMovie />} />
-        <Route path="/member/create" element={<CreateUser />} />
-        <Route path="/member/edit/:userId" element={<EditUser />} />
-        <Route path="/category/create" element={<CreateCategory />} />
-        <Route path="/category/edit/:cateId" element={<UpdateCategory />} />
-        <Route path="/cast/create" element={<CreateCast />} />
-        <Route path="/cast/edit/:castId" element={<UpdateCast />} />
-        <Route path="/cast/detail" element={<DetailCast />} />
-        <Route path="/director/create" element={<CreateDirector />} />
-        <Route path="/director/edit/:directorId" element={<UpdateDirector />} />
-        <Route path="/room/create" element={<CreateRoom />} />
-        <Route path="/room/edit/:roomId" element={<UpdateRoom />} />
-        <Route path="/seat/create" element={<CreateSeat />} />
-        <Route path="/seat/edit/:seatId" element={<UpdateSeat />} />
-        <Route path="/movie_day/create" element={<CreateMovieDay />} />
-      </Routes>
-    </ThemeProvider>
+    <div>
+      {/* {userInfo != null && userInfo !== undefined ? ( */}
+        {/* <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {layout === "dashboard" && (
+            <>
+              <Sidenav
+                color={sidenavColor}
+                brand={brand}
+                brandName="2TNMCinemas"
+                routes={routes}
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+              />
+            </>
+          )}
+          {layout === "vr"}
+
+          <Routes>
+            {getRoutes(routes)}
+            <Route path="*" element={<Navigate to="/authentication/sign-in" />} />
+            <Route path="/authentication/sign-in" element={<SignIn />} />
+            <Route path="/authentication/sign-up" element={<SignUp />} />
+            <Route path="/movies/create" element={<Createnew />} />
+            <Route path="/movies/edit/:movieId" element={<EditMovie />} />
+            <Route path="/movies/detail/:movieId" element={<DetailMovie />} />
+            <Route path="/member/create" element={<CreateUser />} />
+            <Route path="/member/edit/:userId" element={<EditUser />} />
+            <Route path="/category/create" element={<CreateCategory />} />
+            <Route path="/category/edit/:cateId" element={<UpdateCategory />} />
+            <Route path="/cast/create" element={<CreateCast />} />
+            <Route path="/cast/edit/:castId" element={<UpdateCast />} />
+            <Route path="/cast/detail" element={<DetailCast />} />
+            <Route path="/director/create" element={<CreateDirector />} />
+            <Route path="/director/edit/:directorId" element={<UpdateDirector />} />
+            <Route path="/room/create" element={<CreateRoom />} />
+            <Route path="/room/edit/:roomId" element={<UpdateRoom />} />
+            <Route path="/seat/create" element={<CreateSeat />} />
+            <Route path="/seat/edit/:seatId" element={<UpdateSeat />} />
+            <Route path="/movie_day/create" element={<CreateMovieDay />} />
+          </Routes>
+        </ThemeProvider> */}
+      {/* ) : ( */}
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      {/* )} */}
+    </div>
   );
 }

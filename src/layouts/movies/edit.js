a7cf07c50b1b32c5d2102339e76078e4fc3c0cad
Nguyen Van Nam <nam.nv@aris-vn.com>
2023-03-22 15:05:04 +0700

    update link trailler when edit

--- src/layouts/movies/edit.js
+++ src/layouts/movies/edit.js
@@ -168,6 +168,7 @@ const EditMovie = () => {
                             className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-400 focus:border-blue-400 block w-full p-2.5 "
                             id="trailers"
                             placeholder="Link youtube"
+                            value={"https://www.youtube.com/watch?v="+currentMovie.trailer}
                             required
                           />
                         </SoftBox>
